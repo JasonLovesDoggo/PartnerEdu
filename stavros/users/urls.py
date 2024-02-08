933d2e5427a5440bc114d0e3b0d1d96c920c90ff
Jason <git@jasoncameron.dev>
2024-02-08 17:50:55 -0500

    convert to class based views

--- stavros/users/urls.py
+++ stavros/users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 
-from stavros.users.views import user_detail_view, user_redirect_view, user_update_view
-
+from stavros.users.views import UserDetailView, UserRedirectView, UserUpdateView
 app_name = "users"
 urlpatterns = [
     path("", include("pwa.urls")),
-    path("~redirect/", view=user_redirect_view, name="redirect"),
-    path("~update/", view=user_update_view, name="update"),
-    path("<int:pk>/", view=user_detail_view, name="detail"),
+    path("~redirect/", view=UserRedirectView.as_view(), name="redirect"),
+    path("~update/", view=UserUpdateView.as_view(), name="update"),
+    path("<int:pk>/", view=UserDetailView.as_view(), name="detail"),
     path("", include("django.contrib.flatpages.urls")),
 ]
