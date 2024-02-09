137da0e7e6797639253bc3cabbe567df14e0d615
Jason <git@jasoncameron.dev>
2024-02-09 17:29:41 -0500

    added the rest of the views

--- stavros/users/urls.py
+++ stavros/users/urls.py
@@ -1,10 +1,18 @@
-from django.urls import include, path
+from django.urls import path
 
-from stavros.users.views import UserDetailView, UserRedirectView, UserUpdateView
+from stavros.users.views import *
 
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=UserRedirectView.as_view(), name="redirect"),
     path("~update/", view=UserUpdateView.as_view(), name="update"),
-    path("<int:pk>/", view=UserDetailView.as_view(), name="detail"),
+    path("users/<int:pk>/", view=UserDetailView.as_view(), name="detail"),
+    path("", view=DashboardView.as_view(), name="dashboard"),
+    path("events/", view=EventListView.as_view(), name="event_list"),
+    path("events/<int:pk>/", view=EventDetailView.as_view(), name="event_detail"),
+    path("organizations/", view=OrganizationListView.as_view(), name="organization_list"),
+    path("organizations/<slug:slug>/", view=OrganizationDetailView.as_view(), name="organization_detail"),
+    path("recent/", view=AnnouncementListView.as_view(), name="announcement_list"),
+    path("announcement/<slug:slug>/", view=AnnouncementDetailView.as_view(), name="announcement_detail"),
+    path("map", view=MapView.as_view(), name="map"),
 ]
--- stavros/users/views.py
+++ stavros/users/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import IntegerField, When, F, Case
+from django.db.models.fields import DurationField
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import DetailView, RedirectView, UpdateView
+from django.views.generic import DetailView, RedirectView, UpdateView, ListView
+from django.shortcuts import get_object_or_404
+from stavros.users.models import Event, Organization, Announcement
 
 User = get_user_model()
 
@@ -32,3 +37,67 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
     def get_redirect_url(self):
         return reverse("users:detail", kwargs={"pk": self.request.user.pk})
+
+
+class EventListView(ListView):
+    model = Event
+    template_name = "event_list.html"
+    context_object_name = "events"
+    paginate_by = 10
+
+    def get_queryset(self):
+        now = timezone.now()
+        queryset = (
+            Event.objects.annotate(
+                relevance=Case(
+                    When(date__gte=now, then=1),
+                    When(date__lt=now, then=2),
+                    output_field=IntegerField(),
+                )
+            )
+            .annotate(
+                timediff=Case(
+                    When(date__gte=now, then=F("date") - now),
+                    When(date__lt=now, then=now - F("date")),
+                    output_field=DurationField(),
+                )
+            )
+            .order_by("relevance", "timediff")
+        )
+        return queryset
+
+
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = "event_detail.html"
+
+    def get_object(self, queryset=None):
+        id_ = self.kwargs.get("pk")
+        return get_object_or_404(Event, id=id_)
+
+
+class OrganizationDetailView(DetailView):
+    model = Organization
+    template_name = "organization_detail.html"
+
+    def get_object(self, queryset=None):
+        slug_ = self.kwargs.get("slug")
+        return get_object_or_404(Organization, slug=slug_)
+
+class AnnouncementListView(ListView):
+    model = Event
+    template_name = "announcement_list.html"
+    context_object_name = "announcements"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Announcement.objects.filter(date_posted__lte=timezone.now()).order_by('-date')
+
+class AnnouncementDetailView(DetailView):
+    model = Event
+    template_name = "announcement_detail.html"
+
+    def get_object(self, queryset=None):
+        slug_ = self.kwargs.get("slug")
+        return get_object_or_404(Announcement, slug=slug_)
