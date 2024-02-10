fe66774eba45c20ebe28dc4efb0ad5e386b01d78
Jason <git@jasoncameron.dev>
2024-02-09 22:15:38 -0500

    update tgo py312

--- tests/test_production_views_responses.py
+++ tests/test_production_views_responses.py
@@ -1,2 +1,6 @@
-def test_sanity_check():
+def test_sanity_check() -> None:
+    """
+    This is a sanity check test to make sure that the tests are running
+    :return: None
+    """
     assert 1 + 1 == 2
