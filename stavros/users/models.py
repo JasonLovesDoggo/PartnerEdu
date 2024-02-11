84bb0d95dcd8c5937ee6ba96223fc276fccec95d
Jason <git@jasoncameron.dev>
2024-02-10 23:24:48 -0500

    document views and models

--- stavros/users/models.py
+++ stavros/users/models.py
@@ -25,117 +25,210 @@ from stavros.users.utils.choices import COURSE_OPTIONS, ORGANIZATION_TYPES, POSI
 
 class User(AbstractUser):
     """
-    Default custom user model for Stavros.
-    If adding fields that need to be filled at user signup,
-    check forms.SignupForm and forms.SocialSignupForms accordingly.
+    This is the User model. It represents a user in the system.
+    It extends the AbstractUser model provided by Django.
     """
 
-    # First and last name do not cover name patterns around the globe
+    # The name of the user. It can be blank.
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    # The first name and last name fields are not used in this model.
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    # The email of the user. It must be unique.
     email = EmailField(_("email address"), unique=True)
+    # The username field is not used in this model.
     username = None  # type: ignore
 
+    # The email field is used as the username field.
     USERNAME_FIELD = "email"
+    # There are no required fields.
     REQUIRED_FIELDS = []
+    # The tags that the user is subscribed to.
     subscribed_tags = ManyToManyField("Tag")
+    # The organizations that the user is subscribed to.
     subscribed_organizations = ManyToManyField("Organization")
+    # The contacts of the user.
     contacts = ManyToManyField("Contact", blank=True, related_name="contacts")
-    # is teacher is if they are in the teacher group
+    # The manager for this model.
     objects: ClassVar[UserManager] = UserManager()
 
     def get_absolute_url(self) -> str:
-        """Get URL for user's detail view.
-
-        Returns:
-            str: URL for user detail.
-
+        """
+        This method returns the URL for the detail view of the user.
         """
         return reverse("users:detail", kwargs={"pk": self.id})
 
 
 class Class(Model):
+    """
+    This is the Class model. It represents a class in a school.
+    """
+
+    # The name of the class.
     name = CharField(max_length=255)
+    # The subject of the class.
     subject = CharField(max_length=255, choices=COURSE_OPTIONS)
+    # The grade level of the class.
     grade_level = IntegerField(choices=[(i, i) for i in range(9, 13)])
+    # The teacher of the class.
     teacher = ForeignKey(User, on_delete=CASCADE, related_name="classes_teaching")
+    # The students attending the class.
     students = ManyToManyField("StudentProfile", related_name="classes_attending", blank=True)
 
 
 class StudentProfile(Model):
+    """
+    This is the StudentProfile model. It represents a student's profile.
+    """
+
+    # The user associated with this profile.
     user = ForeignKey(User, on_delete=CASCADE, related_name="student_profile")
+    # The birth date of the student.
     birth_date = DateTimeField()
+    # The address of the student.
     address = PlainLocationField()
+    # The year the student is graduating.
     graduating_year = CharField(max_length=255, choices=[(i, i) for i in range(2022, 2030)])
+    # The unique ID of the student.
     student_id = CharField(unique=True, max_length=9)  # 9 digit student id
+    # Any additional notes about the student.
     notes = TextField(blank=True, null=True)
+    # The guidance counselor of the student.
     guidance_counselor = ForeignKey(User, on_delete=CASCADE, related_name="students", null=True, blank=True)
+    # The classes that the student has taken.
     classes_taken = ManyToManyField("Class")
+    # The contact information of the student's parents.
     parental_contact = ForeignKey("Contact", on_delete=CASCADE, related_name="children", null=True, blank=True)
 
 
 class Resource(Model):
+    """
+    This is the Resource model. It represents a resource.
+    """
+
+    # The title of the resource.
     title = CharField(max_length=255)
+    # Any additional information about the resource.
     additional_info = TextField()
+    # The link to the resource.
     link = URLField(null=True, blank=True)
+    # The tags associated with the resource.
     tags = ManyToManyField("Tag", related_name="resources")
 
 
 class Contact(Model):
+    """
+    This is the Contact model. It represents a contact.
+    """
+
+    # The internal name of the contact.
     internal_name = CharField(max_length=255)
+    # The user associated with this contact.
     user = ForeignKey(User, on_delete=CASCADE, related_name="info")
+    # The position of the contact in their company.
     company_position = CharField(max_length=255, blank=True, choices=POSITIONS)
+    # The phone number of the contact.
     phone_regex = RegexValidator(
         regex=r"^\+?1?\d{9,15}$",  # only allow proper phone numbers to be entered
         message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.",
     )
+    # Any additional notes about the contact.
     notes = TextField(blank=True, null=True)
+    # The phone number of the contact.
     phone_number = CharField(validators=[phone_regex], max_length=17, blank=True)
+    # The industry of the contact.
     industry = CharField(max_length=255, blank=True)
+    # The tags associated with the contact.
     tags = ManyToManyField("Tag", blank=True, related_name="contacts")
 
 
 class Announcement(Model):
+    """
+    This is the Announcement model. It represents an announcement.
+    """
+
+    # The unique slug of the announcement.
     slug = SlugField(unique=True, editable=False, null=True, blank=True)
+    # The title of the announcement.
     title = CharField(max_length=255)
+    # The content of the announcement.
     content = TextField()
+    # The date the announcement was posted.
     date_posted = DateTimeField(auto_now_add=True, editable=False)
+    # The tags associated with the announcement.
     tags = ManyToManyField("Tag", related_name="announcements")
+    # The organization that posted the announcement.
     organization = ForeignKey("Organization", on_delete=CASCADE, related_name="announcements")
 
     def save(self, *args, **kwargs):
+        """
+        This method is called when the announcement is saved.
+        It generates a unique slug for the announcement.
+        """
         if not self.id or not self.slug:  # only on creation
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
 
 class Organization(Model):
+    """
+    This is the Organization model. It represents an organization.
+    """
+
+    # The name of the organization.
     name = CharField(max_length=200)
+    # The unique slug of the organization.
     slug = SlugField(unique=True, editable=False, null=True, blank=True)
+    # The category of the organization.
     category = CharField(max_length=255, choices=[(i, i) for i in ORGANIZATION_TYPES])
+    # The type of events the organization hosts.
     event_type = CharField(max_length=255)
+    # The resources associated with the organization.
     resources = ManyToManyField("Resource", related_name="organizations", blank=True)
+    # The contacts associated with the organization.
     contacts = ManyToManyField("Contact", blank=True)
+    # The tags associated with the organization.
     tags = ManyToManyField("Tag", related_name="organizations")
 
     def save(self, *args, **kwargs):
+        """
+        This method is called when the organization is saved.
+        It generates a unique slug for the organization.
+        """
         if not self.id or not self.slug:  # only on creation
             self.slug = slugify(self.name)  # replace spaces with hyphens and other unicode changes.
         super().save(*args, **kwargs)
 
 
 class Event(Model):
+    """
+    This is the Event model. It represents an event.
+    """
+
+    # The name of the event.
     name = CharField(max_length=255)
+    # Any additional information about the event.
     info = TextField()
+    # The start date of the event.
     start_date = DateTimeField()
+    # The end date of the event.
     end_date = DateTimeField()
+    # The organization that is hosting the event.
     organization = ForeignKey(Organization, on_delete=CASCADE, related_name="events")
+    # The city where the event is taking place.
     city = CharField(max_length=40)
+    # The location of the event.
     location = PlainLocationField(based_fields=["city"], zoom=7)
+    # The tags associated with the event.
     tags = ManyToManyField("Tag", related_name="events")
+    # The users who are attending the event.
     attendees = ManyToManyField(User, related_name="events", blank=True)
 
 
 class Tag(Model):
+    """
+    This is the Tag model. It represents a tag.
+    """
+
+    # The name of the tag.
     name = CharField(max_length=255)
--- stavros/users/views.py
+++ stavros/users/views.py
@@ -15,43 +15,78 @@ from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 import stavros
 from stavros.users.models import Announcement, Event, Organization
 
+# Get the user model from Django's built-in user model
 User = get_user_model()
 
 
 class UserDetailView(LoginRequiredMixin, DetailView):
-    model = User
-    slug_field = "id"
-    slug_url_kwarg = "id"
+    """
+    This view is for displaying the details of a user.
+    It requires the user to be logged in to view their details.
+    """
+
+    model = User  # The model being used is the User model
+    slug_field = "id"  # The field in the User model that's used for the lookup is 'id'
+    slug_url_kwarg = "id"  # The name of the keyword argument in the URLconf is 'id'
 
 
 class UserUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
-    model = User
-    fields = ["name"]
-    success_message = _("Information successfully updated")
+    """
+    This view is for updating the details of a user.
+    It requires the user to be logged in to update their details.
+    Upon successful update, a success message is displayed.
+    """
+
+    model = User  # The model being used is the User model
+    fields = ["name"]  # The field that can be updated is 'name'
+    success_message = _("Information successfully updated")  # The message displayed upon successful update
 
     def get_success_url(self):
+        """
+        This method returns the URL to redirect to after the user's details are successfully updated.
+        """
         assert self.request.user.is_authenticated  # for mypy to know that the user is authenticated
-        return self.request.user.get_absolute_url()
+        return self.request.user.get_absolute_url()  # The URL to redirect to is the user's detail view
 
     def get_object(self):
-        return self.request.user
+        """
+        This method returns the User object to be updated.
+        """
+        return self.request.user  # The User object to be updated is the currently logged in user
 
 
 class UserRedirectView(LoginRequiredMixin, RedirectView):
-    permanent = False
+    """
+    This view is for redirecting the user to their detail view.
+    It requires the user to be logged in to be redirected.
+    """
+
+    permanent = False  # This is not a permanent redirect
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"pk": self.request.user.pk})
+        """
+        This method returns the URL to redirect to.
+        """
+        return reverse(
+            "users:detail", kwargs={"pk": self.request.user.pk}
+        )  # The URL to redirect to is the user's detail view
 
 
 class EventListView(ListView):
-    model = Event
-    template_name = "event_list.html"
-    context_object_name = "events"
-    paginate_by = 10
+    """
+    This view is for displaying a list of events.
+    """
+
+    model = Event  # The model being used is the Event model
+    template_name = "event_list.html"  # The template used for this view is 'event_list.html'
+    context_object_name = "events"  # The name of the variable to be used in the template context is 'events'
+    paginate_by = 10  # The number of events displayed per page is 10
 
     def get_queryset(self):
-        now = timezone.now()
+        """
+        This method returns the queryset to be used for the list view.
+        """
+        now = timezone.now()  # Get the current time
         queryset = (
             Event.objects.annotate(
                 relevance=Case(
@@ -69,70 +104,129 @@ class EventListView(ListView):
             )
             .order_by("relevance", "timediff")
         )
-        return queryset
+        return queryset  # Return the queryset
 
 
 class EventDetailView(DetailView):
-    model = Event
-    template_name = "event_detail.html"
+    """
+    This view is for displaying the details of an event.
+    """
+
+    model = Event  # The model being used is the Event model
+    template_name = "event_detail.html"  # The template used for this view is 'event_detail.html'
 
     def get_object(self, queryset=None):
-        id_ = self.kwargs.get("pk")
-        return get_object_or_404(Event, id=id_)
+        """
+        This method returns the Event object to be shown in detail view.
+        """
+        id_ = self.kwargs.get("pk")  # Get the primary key from the URL
+        return get_object_or_404(Event, id=id_)  # Return the Event object with the given primary key
 
 
 class OrganizationListView(ListView):
-    model = Organization
-    template_name = "organization_list.html"
-    context_object_name = "organizations"
-    paginate_by = 10
+    """
+    This view is for displaying a list of organizations.
+    """
+
+    model = Organization  # The model being used is the Organization model
+    template_name = "organization_list.html"  # The template used for this view is 'organization_list.html'
+    context_object_name = (
+        "organizations"  # The name of the variable to be used in the template context is 'organizations'
+    )
+    paginate_by = 10  # The number of organizations displayed per page is 10
 
     def get_queryset(self):
-        return Organization.objects.all()
+        """
+        This method returns the queryset to be used for the list view.
+        """
+        return Organization.objects.all()  # Return all Organization objects
 
 
 class OrganizationDetailView(DetailView):
-    model = Organization
-    template_name = "organization_detail.html"
+    """
+    This view is for displaying the details of an organization.
+    """
+
+    model = Organization  # The model being used is the Organization model
+    template_name = "organization_detail.html"  # The template used for this view is 'organization_detail.html'
 
     def get_object(self, queryset=None):
-        slug_ = self.kwargs.get("slug")
-        return get_object_or_404(Organization, slug=slug_)
+        """
+        This method returns the Organization object to be shown in detail view.
+        """
+        slug_ = self.kwargs.get("slug")  # Get the slug from the URL
+        return get_object_or_404(Organization, slug=slug_)  # Return the Organization object with the given slug
 
 
 class AnnouncementListView(ListView):
-    model = Event
-    template_name = "announcement_list.html"
-    context_object_name = "announcements"
-    paginate_by = 10
+    """
+    This view is for displaying a list of announcements.
+    """
+
+    model = Event  # The model being used is the Event model
+    template_name = "announcement_list.html"  # The template used for this view is 'announcement_list.html'
+    context_object_name = (
+        "announcements"  # The name of the variable to be used in the template context is 'announcements'
+    )
+    paginate_by = 10  # The number of announcements displayed per page is 10
 
     def get_queryset(self):
-        return Announcement.objects.filter(date_posted__lte=timezone.now()).order_by("-date")
+        """
+        This method returns the queryset to be used for the list view.
+        """
+        return Announcement.objects.filter(date_posted__lte=timezone.now()).order_by(
+            "-date"
+        )  # Return all Announcement objects that were posted before the current time, \
+        # ordered by date in descending order
 
 
 class AnnouncementDetailView(DetailView):
-    model = Event
-    template_name = "announcement_detail.html"
+    """
+    This view is for displaying the details of an announcement.
+    """
+
+    model = Event  # The model being used is the Event model
+    template_name = "announcement_detail.html"  # The template used for this view is 'announcement_detail.html'
 
     def get_object(self, queryset=None):
-        slug_ = self.kwargs.get("slug")
-        return get_object_or_404(Announcement, slug=slug_)
+        """
+        This method returns the Announcement object to be shown in detail view.
+        """
+        slug_ = self.kwargs.get("slug")  # Get the slug from the URL
+        return get_object_or_404(Announcement, slug=slug_)  # Return the Announcement object with the given slug
 
 
 class DashboardView(LoginRequiredMixin, View):
+    """
+    This view is for displaying the dashboard of the logged in user.
+    It requires the user to be logged in to view their dashboard.
+    """
+
     def get(self, request, *args, **kwargs):
-        user: stavros.users.models.User = request.user
+        """
+        This method handles the GET request.
+        """
+        user: stavros.users.models.User = request.user  # Get the currently logged in user
         context = {
-            "user": user,
-            "events": user.events.all(),
-            "subscribed_orgs": user.subscribed_organizations.all(),
-            "followed_tags": user.subscribed_tags.all(),
+            "user": user,  # The user to be displayed in the dashboard
+            "events": user.events.all(),  # The events of the user
+            "subscribed_orgs": user.subscribed_organizations.all(),  # The organizations that the user is subscribed to
+            "followed_tags": user.subscribed_tags.all(),  # The tags that the user is following
         }
-        return render(request, "dashboard.html", context)
+        return render(request, "dashboard.html", context)  # Render the dashboard with the given context
 
 
 class MapView(View):
+    """
+    This view is for displaying a map with the locations of all organizations.
+    """
+
     def get(self, request, *args, **kwargs):
-        locations = [[o.name, o.latitude, o.longitude, i] for i, o in enumerate(Organization.objects.all())]
-        context = {"locations": json.dumps(locations)}
-        return render(request, "core/map_view.html", context)
+        """
+        This method handles the GET request.
+        """
+        locations = [
+            [o.name, o.latitude, o.longitude, i] for i, o in enumerate(Organization.objects.all())
+        ]  # Get the name, latitude, longitude, and index of all organizations
+        context = {"locations": json.dumps(locations)}  # Convert the locations to JSON and add them to the context
+        return render(request, "core/map_view.html", context)  # Render the map with the given context
